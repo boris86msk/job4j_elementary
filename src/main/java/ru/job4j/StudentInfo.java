04d8ee93b1e35f619820bd59d6a5739423c7aa88
User <boris86msk@yandex.ru>
2022-03-20 23:18:19 +0300

    Ch

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Pokidov Boris");
+        System.out.println("19.03.22");
+    }
+}
