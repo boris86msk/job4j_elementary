3f05fcfd9a5bcee52670c4673fe26e1eea707dc8
User <boris86msk@yandex.ru>
2022-03-29 10:27:52 +0300

    Table

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -2,12 +2,12 @@ package ru.job4j.array;
 
 public class Matrix {
     public static int[][] multiple(int size) {
-        int[][] multiplicationtable = new int[size][size];
+        int[][] table = new int[size][size];
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
-                multiplicationtable[row][cell] = (row + 1) * (cell + 1);
+                table[row][cell] = (row + 1) * (cell + 1);
             }
         }
-        return multiplicationtable;
+        return table;
     }
 }
