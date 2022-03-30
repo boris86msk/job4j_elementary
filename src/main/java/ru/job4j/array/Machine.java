1f1a711f0d0178fe6584f324f7a2f85d041095ab
User <boris86msk@yandex.ru>
2022-03-30 22:16:03 +0300

    дача вкофемашине исправлено

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -8,13 +8,11 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         int cash = money - price;
-            for (int i = 0; i < coins.length; i++) {
-                if (cash / coins[i] > 0) {
-                    for (int j = 0; j <= cash / coins[i]; j++) {
-                        rsl[size] = coins[i];
-                        size++;
-                        cash -= coins[i];
-                    }
+            for (int coin: coins) {
+                while (cash >= coin) {
+                    rsl[size] = coin;
+                    size++;
+                    cash -= coin;
                 }
                 if (cash <= 0) {
                     break;
